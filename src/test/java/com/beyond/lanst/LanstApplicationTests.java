646e0656b11fb0fa87d7be491ed5a9d162e610e1
sueroku <eraltndusn@gmail.com>
2024-07-31 14:45:09 +0900

    start

---
+++ src/test/java/com/beyond/lanst/LanstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.lanst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
